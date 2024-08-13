3114be34dec36697ab69a4028564a62e130a0160
u-sour <145416126+u-sour@users.noreply.github.com>
2024-08-13 08:06:49 +0700

    Update responsive_layout.dart

--- lib/utils/responsive/responsive_layout.dart
+++ lib/utils/responsive/responsive_layout.dart
@@ -13,25 +13,37 @@ class ResponsiveLayout extends StatelessWidget {
   });
 
   // Breakpoint Platform
-  static const int _mobileBreakPoint = 500;
-  static const int _tabletBreakPoint = 1100;
+  static const double _mobileBreakPoint = 600.0;
+  static const double _tabletBreakPoint = 768.0;
+  static const double _desktopBreakPoint = 1440.0;
 
   // isMobile, isTablet, isDesktop helper
+  // isMobile Condition : orientation == portrait and device width smaller than tablet breakpoint
+  // or orientation == landscape and device height smaller than mobile breakpoint
   static bool isMobile(BuildContext context) =>
-      MediaQuery.of(context).size.width < _mobileBreakPoint;
+      MediaQuery.orientationOf(context) == Orientation.portrait &&
+          MediaQuery.sizeOf(context).width < _tabletBreakPoint ||
+      MediaQuery.orientationOf(context) == Orientation.landscape &&
+          MediaQuery.sizeOf(context).height < _mobileBreakPoint;
 
   static bool isTablet(BuildContext context) =>
-      MediaQuery.of(context).size.width < _tabletBreakPoint;
+      MediaQuery.sizeOf(context).width >= _tabletBreakPoint &&
+      MediaQuery.sizeOf(context).width < _desktopBreakPoint;
 
   static bool isDesktop(BuildContext context) =>
-      MediaQuery.of(context).size.width > _tabletBreakPoint;
+      MediaQuery.sizeOf(context).width > _desktopBreakPoint;
 
   @override
   Widget build(BuildContext context) {
+    Orientation orientation = MediaQuery.orientationOf(context);
     return LayoutBuilder(builder: (context, constraints) {
-      if (constraints.maxWidth < _mobileBreakPoint) {
+      if (orientation == Orientation.portrait &&
+              constraints.maxWidth < _tabletBreakPoint ||
+          orientation == Orientation.landscape &&
+              constraints.maxHeight < _mobileBreakPoint) {
         return mobileScaffold;
-      } else if (constraints.maxWidth < _tabletBreakPoint) {
+      } else if (constraints.maxWidth >= _tabletBreakPoint &&
+          constraints.maxWidth < _desktopBreakPoint) {
         return tabletScaffold;
       } else {
         return desktopScaffold;
